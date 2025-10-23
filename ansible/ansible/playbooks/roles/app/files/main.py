638ed65ccdbcaf3421433cbace89bb5123e1a8b9
p.korshunov <korshunov.petr@gmail.com>
2025-10-23 21:50:59 +0300

    add ansible files

---
+++ ansible/ansible/playbooks/roles/app/files/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return '<h1>Hello from Docker!</h1>'
+
+@app.route('/health', methods=['GET'])
+def health():
+    return {'status': 'healthy'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
